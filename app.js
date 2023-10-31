da156dd9d4115b02fcb66a31b3aa6c3f2487d42c
vishalprabhu2018 <prabhuvishal2018@gmail.com>
2023-10-31 11:43:27 +0530

    done with the basic style

---
+++ app.js
@@ -0,0 +1,9 @@
+//selectors
+const todoInput=document.querySelector('.todo-input');
+const todoButton=document.querySelector('.todo-input');
+const todoList=document.querySelector('.todo-input');
+
+//event listerner
+
+
+//functions
